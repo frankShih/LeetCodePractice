53f6db914fd6e2c740d28ccf24155c3b49836dc3
ueasus <ueasus@ueasusde-MacBook-Pro.local>
2019-01-02 16:47:18 +0800

    add aStar algo.

--- practice_searching/dijkstra.py
+++ practice_searching/dijkstra.py
@@ -44,21 +44,13 @@ dijkstraAlgo(graph.directGraph, "a")
 
 def dijkstraAlgoMatrix(start=0):
     #graph = np.array([[0,8,3,5,100],[8,0,2,100,5],[100,1,0,3,4],[6,100,150,0,7],[100,5,100,100,0]])
-    graph = np.array([[0,2,100,3,100,100,100],[100,0,7,100,1,100,100],[100,100,0,100,100,2,100],[100,1,100,0,100,100,100],[100,100,7,100,0,100,4],[100,100,100,100,4,0,2],[1,100,100,1,100,100,0]])
+    graph = np.array([[0,2,45,3,22,54,33],[77,0,7,98,66,11,89],[78,56,0,11,23,2,87],[32,1,111,0,9,65,68],[44,34,7,86,0,43,4],[22,35,21,12,4,0,44],[1,11,53,88,76,67,0]])
+
     #graph = np.array([[0,3,8,100,-4],[100,0,100,1,7],[100,4,0,100,100],[2,100,-5,0,100],[100,100,100,6,0]])
-
-
 
     print("Adjacency matrix")
     print(graph)
     v = len(graph)
-
-    # path reconstruction matrix
-    p = np.zeros(graph.shape)
-    for i in range(0,v):
-        for j in range(0,v):
-            p[i,j] = graph[i,j]
-            
     visited = [start]
 
     while len(visited)!= len(graph): 
@@ -68,9 +60,11 @@ def dijkstraAlgoMatrix(start=0):
             #since it is full-connected graph, no need to create a set of unvisited "neighbours"
             if k in visited:
                 continue
-            if p[start, k]<best:
+
+            # evaluate function: cost from start point    
+            if graph[start, k]<best:
                 bestInd = k
-                best = p[start, k]
+                best = graph[start, k]
 
         visited.append(bestInd)
         start = bestInd
