2b973b2eacfaa4c9910870390fbb6000b6028edc
ueasus <ueasus@ueasusde-MacBook-Pro.local>
2019-01-09 17:49:26 +0800

    add wordBreakII problem. update wordBreak problem

--- 139-workBreak/solution.py
+++ 139-workBreak/solution.py
@@ -11,16 +11,21 @@ class Solution(object):
 
         length = len(s)
 
-        # recursive version O(M^N), M:dict size; N length of 's'
-        '''
+        # recursive version with memorization. O(M*N^2)->O(M*N), M:dict size; N length of 's'
+        mem = {}
         def helper(remain):
             if not remain: 
                 return True
 
+            if remain in mem:
+                return mem[remain]
+            
             result = False
             for w in wordDict:
                 if remain.startswith(w):
-                    result |= helper(remain[len(w):])
+                    temp = helper(remain[len(w):])
+                    mem[remain[len(w):]] = temp
+                    result |= temp
 
             return result
             
@@ -28,7 +33,7 @@ class Solution(object):
 
         '''
 
-        # DP, with memory optimized, 30% (unbounded knapsack problem)
+        # DP O(M*N), with memory optimized, 30% (unbounded knapsack problem)
         dp = [False]*(length+1)
         dp[0] = True
         for i in range(1, length+1):
@@ -39,3 +44,4 @@ class Solution(object):
                     dp[i] |= dp[i-ll]
 
         return dp[-1]
+        '''
--- 140-wordBreakII/solution.py
+++ 140-wordBreakII/solution.py
@@ -5,9 +5,9 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: List[str]
         """
-            
         wordSet = set(wordDict)
-        # naive BFS, O(M^N)
+        '''
+        # naive BFS, O(M*N^2), timeout
         Q = [[s, []]]
         result = []
         while Q:
@@ -17,15 +17,31 @@ class Solution(object):
                 result.append(" ".join(path))
                 continue
 
-            for w in wordDict:
-                if remain.startswith(w):
-                    Q.append([remain[len(w):], path+[w]])
-            # length = len(remain)
-            # for i in range(length+1):
-            #     if remain[:i] in wordSet:
-            #         Q.append([remain[i:], path+[remain[:i]]])
-
+            length = len(remain)
+            for i in range(length+1):
+                if remain[:i] in wordSet:
+                    Q.append([remain[i:], path+[remain[:i]]])
 
         return result
+        '''
 
-
+        DP recursive O(N*M^2)
+        memo={}
+        def go(s):
+            if s in memo: return memo[s]
+            if not s: return []
+            ret = []
+            for w in wordSet:
+                if not s.startswith(w): continue
+                elif len(w)==len(s): ret.append(w)
+                else:
+                    rest = go(s[len(w):])
+                    print(w, rest)
+                    for r in rest:
+                        r = w + " " + r
+                        ret.append(r)
+                    
+            memo[s] = ret
+            return ret
+        
+        return go(s)
