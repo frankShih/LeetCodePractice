f1779d0bec5cea7111a8f9b3850aa3c05803af7c
Han Shih(施學翰) <Han_Shih@corpnet.asus>
2019-01-22 17:53:56 +0800

    add binaryTreePruning, constructBTfromPreorderAndInorderTraversal problem

--- 110-balancedBinaryTree/solution.py
+++ 110-balancedBinaryTree/solution.py
@@ -5,31 +5,30 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
 
+class Solution(object):
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        if not(root):
+        if not root:
             return True
 
         def treeH(root):
-            if not(root):
+            if not (root):
                 return 0
 
-            ll = treeH(root.left)
-            rr = treeH(root.right)
-            return 1+max(ll, rr)
+            ll, rr = treeH(root.left), treeH(root.right)
+            return 1 + max(ll, rr)
 
-        if abs(treeH(root.right) - treeH(root.left))>1:
+        if abs(treeH(root.right) - treeH(root.left)) > 1:
             return False
 
+        # check if there exist unbalanced case in subtrees
         return self.isBalanced(root.left) and self.isBalanced(root.right)
 
-
-        '''
+        """
         def height(node):
             if not node:
                 return 0
@@ -40,4 +39,4 @@ class Solution(object):
 
         # None happens when tree is unbalanced so nothing return
         return height(root)!=None
-        '''
+        """
